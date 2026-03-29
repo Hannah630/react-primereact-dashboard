ec230a7a2e743de6d3075da117e903d070870452
Hannah630 <cc20010630@gmail.com>
2026-03-29 18:50:20 +0800

    fix: add types for css and images

---
+++ frontend/src/vite-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+// CSS Modules
+declare module '*.module.css';
+
+// 圖片
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
